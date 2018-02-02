81f59c575759a5b16f114e2d405c6fc7fc1eb13c
John Comeau <jc@unternet.net>
2018-02-02 08:53:28 -0800

    taking to next phase

--- Makefile
+++ Makefile
@@ -1,18 +1,23 @@
+SHELL := /bin/bash
 PREFIX := crypto-escrow-sandbox
 INSECURE ?= --insecure
-KEY := $(PREFIX).$(SERVER)
+KEY := $(PREFIX).$(SERVER_NAME)
 NETRC := $(HOME)/.netrc
-export  # must first `export SERVER=example.com` in shell before `make deploy`
+SAMPLE := $(shell echo "$$(<sample_transaction.json)")
+export  # `export SERVER_NAME=example.com` in shell before `make deploy`
 default: test2 test3
 test2:
 	echo test | python2 paypal_ipn.py
 test3:
 	echo test | python3 paypal_ipn.py
+env set:
+	$@
 deploy: paypal_ipn.py
-	scp paypal_ipn.py $(SERVER):/var/www/$(SERVER)/tmp/$(<:.py=.cgi)
+	scp paypal_ipn.py \
+	 $(SERVER_NAME):/var/www/$(SERVER_NAME)/tmp/$(<:.py=.cgi)
 remote: paypal_ipn.py deploy
 	# to check certificates, `make INSECURE= remote`
-	curl $(INSECURE) --data foo=bar https://$(SERVER)/tmp/$(<:.py=.cgi)
+	curl $(INSECURE) --data foo=bar https://$(SERVER_NAME)/tmp/$(<:.py=.cgi)
 # the following rules require an entry, all on one line, in $HOME/.netrc
 # machine crypto-escrow-sandbox.example.com
 #  login APPID password APPKEY account TOKEN
@@ -32,4 +37,4 @@ payment:
 	curl --verbose https://api.sandbox.paypal.com/v1/payments/payment \
 	 --header "Content-Type: application/json" \
 	 --header "Authorization: Bearer $$token" \
-	 --data '{"intent": "sale", "redirect_urls": {"return_url": "https://example.com/your_redirect_url.html", "cancel_url": "https://example.com/your_cancel_url.html"}, "payer": {"payment_method": "paypal"}, "transactions": [{"amount": {"total": "7.47", "currency": "USD"}}]}'
+	 --data '$(SAMPLE)'
--- paypal_ipn.py
+++ paypal_ipn.py
@@ -5,6 +5,7 @@ This module processes PayPal Instant Payment Notification messages (IPNs).
 from __future__ import print_function
 
 import sys
+import os
 import cgi
 import smtplib
 import logging
@@ -21,10 +22,15 @@ except ImportError:  # Python3
 
 VERIFY_URL_PROD = 'https://www.paypal.com/cgi-bin/webscr'
 VERIFY_URL_TEST = 'https://www.sandbox.paypal.com/cgi-bin/webscr'
+# non-sandbox useragent should be "PayPal IPN ( https://www.paypal.com/ipn )"
+NON_SANDBOX = 'https://www.paypal.com/ipn'
+USERAGENT = os.getenv('HTTP_USER_AGENT', '')
+VERIFY_URL = VERIFY_URL_PROD if NON_SANDBOX in USERAGENT else VERIFY_URL_TEST
+SERVER_NAME = os.getenv('SERVER_NAME', 'localhost')
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 logging.debug('logging at DEBUG level')
 
-def paypal_ipn(verify_url=VERIFY_URL_TEST):
+def paypal_ipn():
     '''
     verify PayPal transaction
     '''
@@ -34,21 +40,23 @@ def paypal_ipn(verify_url=VERIFY_URL_TEST):
     parameters.append(('cmd', '_notify-validate'))
     postdata = urlencode(dict(parameters)).encode('utf8')
     logging.info('postdata: %s', postdata)
-    request = Request(verify_url)
+    request = Request(VERIFY_URL)
     # Post back to PayPal for validation
-    logging.info('posting to %s', verify_url)
+    logging.info('posting to %s', VERIFY_URL)
     response = urlopen(request, postdata).read().decode('utf8')
     logging.info('response: %s', response)
     if response == 'VERIFIED':
         logging.info('valid transaction')
-        sendmail(postdata)
+        sendmail('successful PayPal transaction', postdata)
     elif response == 'INVALID':
         logging.warning('invalid transaction')
+        sendmail('failed PayPal transaction', postdata)
     else:
         logging.error('unexpected response: %s', response)
+        sendmail('unknown PayPal transaction', postdata)
     print('IPN session complete.')
 
-def sendmail(contents):
+def sendmail(subject, contents):
     '''
     send a mail to PayPal recipient or server administrator
     '''
@@ -56,13 +64,16 @@ def sendmail(contents):
         ipn_dict = dict(urlparse.parse_qsl(contents))
     except (TypeError, ValueError):
         ipn_dict = {}
-    to_addr = [ipn_dict.get('receiver_email', 'root@localhost')]
+    if 'success' in subject:
+        to_addr = [ipn_dict.get('receiver_email', 'root@%s' % SERVER_NAME)]
+    else:  # don't let people use this for spamming
+        to_addr = ['root@%s' % SERVER_NAME]
     from_addr = 'paypal_ipn@%s' % to_addr[0].split('@')[1]
     server = smtplib.SMTP('localhost')
     message = [
         'From: %s\n' % from_addr,
         'To: %s\n' % to_addr[0],
-        'Subject: PayPal order received\n',
+        'Subject: %s\n' % subject,
         '\n',
         'Received order: %s\n' % contents,
     ]
